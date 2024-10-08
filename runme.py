757f42be542e84988f06caf7404292264ff30a86
Cristian Canton Ferrer <ccanton@fb.com>
2024-10-08 17:02:16 +0200

    Updates to cache

--- runme.py
+++ runme.py
@@ -9,6 +9,7 @@ from io import BytesIO
 import json
 import random
 import time
+import hashlib
 
 import requests
 from bs4 import BeautifulSoup
@@ -24,35 +25,53 @@ FETCH_INTERVAL = 3
 QUEUE_SIZE = 3
 
 def load_image_from_url(image_url):
-    response = requests.get(image_url)
-    image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
-    return cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    try:
+        response = requests.get(image_url)
+        image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+        img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        logging.info(f"Loaded image {image_url}")
+    except Exception as e:
+        logging.critical(f"An error occurred loading image {image_url}: {e}")
+
+    return img
+
+
+def load_image(image_path):
+    img = cv2.imread(image_path)
+    if img is None:
+        logging.error(f"{image_path} not found.")
+    else:
+        logging.info(f"{image_path} loaded successfully.")
+    
+    return img
 
 class ImageFetcher(threading.Thread):
-    def __init__(self, queue, urls):
+    def __init__(self, queue, media_repo):
         threading.Thread.__init__(self)
         self.queue = queue
-        self.urls = urls
+        self.media_repo = media_repo
 
     def run(self):
         while True:
             
-            random.shuffle(self.urls)
+            try:
+                image = self.media_repo.next_item()
+                self.queue.put(image)
+            except Exception as e:
+                logging.info(f"Error fetching image: {e}")
 
-            for url in self.urls:
-                try:
-                    image = load_image_from_url(url)
-                    self.queue.put(image)
-                    logging.info(f"Fetched image from {url}")
-                except Exception as e:
-                    logging.info(f"Error fetching image from {url}: {e}")
-                time.sleep(FETCH_INTERVAL)
+            time.sleep(FETCH_INTERVAL)
 
 
 class URLFountain:
     def	__init__(self, url):
         self.type = 'URL' 
         self.url = url
+        self.items = None
+
+        self.update_files()
+
+    def update_files(self):
         self.items = self.get_files()
 
     def get_files(self):
@@ -76,84 +95,132 @@ class URLFountain:
             logging.critical(f"An error occurred: {e}")
             return []
     
-class LocalMediaRepository:
+class MediaRepository:
 
-    def __init__(self):
+    def __init__(self, monitor_width, monitor_height):
         self.local_ledger_filename = 'image_repository.json'
-        self.local_ledger = None
-        
+        self.local_ledger = {}
+        self.monitor_width, self.monitor_height = monitor_width, monitor_height
+
+    def internal_hash(self, input_string):
+        return hashlib.md5(input_string.encode()).hexdigest()
+    
+    def save_local_ledger(self):
+        with open(self.local_ledger_filename, "w") as file:
+            json.dump(self.local_ledger, file, indent=4)
+
     def load_local_ledger(self):
         if os.path.isfile(self.local_ledger_filename):
             with open(self.local_ledger_filename, 'r') as f:
                 self.local_ledger = json.load(f)
                 logging.info(f"Read {self.local_ledger_filename}")
         else:
-            self.local_ledger = []
+            self.local_ledger = {}
             logging.info(f"Ledger file {self.local_ledger_filename} not present. Initialized to empty.")
 
-    def update(self, )
-    
+    def update(self, url_fountain):
+        url_fountain.update_files()
+        new_files = url_fountain.items
+
+        set_filenames_hashed = []
+
+        # Add new images
+
+        for curr_file in new_files:
+            filename_hashed = self.internal_hash(curr_file)
+
+            set_filenames_hashed.append(filename_hashed)
+
+            if filename_hashed not in self.local_ledger:
+                data = {}
+                data['url'] = curr_file
+                data['local_path'] = f'{filename_hashed}.jpg'
+                self.local_ledger[filename_hashed] = data
+            
+        # Remove images not in url_fountain
+        keys_to_remove = []
+        for curr_hash in self.local_ledger.keys():
+            if curr_hash in set_filenames_hashed:
+                continue
+            
+            keys_to_remove.append(curr_hash)
+
+        for curr_hash in keys_to_remove:
+
+            logging.info(f"Removed {curr_hash} --> {self.local_ledger[curr_hash]['url']}")
+
+            if os.path.exists(self.local_ledger[curr_hash]['local_path']):
+                os.remove(self.local_ledger[curr_hash]['local_path'])
+                logging.info(f"Deleted {self.local_ledger[curr_hash]['local_path']}")
+
+            del self.local_ledger[curr_hash]
+
+    def prepare_image(self, image):
+        # Get image dimensions
+        image_height, image_width, _ = image.shape
+
+        # Calculate aspect ratio
+        image_aspect_ratio = image_width / image_height
+        monitor_aspect_ratio = self.monitor_width / self.monitor_height
+
+        # Calculate new dimensions to fill the screen while maintaining aspect ratio
+        if image_aspect_ratio > monitor_aspect_ratio:
+            new_width = self.monitor_width
+            new_height = int(self.monitor_width / image_aspect_ratio)
+        else:
+            new_width = int(self.monitor_height * image_aspect_ratio)
+            new_height = self.monitor_height
+
+        # Resize the image
+        resized_image = cv2.resize(image, (new_width, new_height))
+
+        # Calculate padding to center the image
+        pad_x = (self.monitor_width - new_width) // 2
+        pad_y = (self.monitor_height - new_height) // 2
+
+        # Create a black background to fill the entire screen
+        background = np.zeros((self.monitor_height, self.monitor_width, 3), dtype=np.uint8)
+
+        # Place the resized image in the center of the background
+        background[pad_y:pad_y+new_height, pad_x:pad_x+new_width] = resized_image
+
+        return background
+
+    def load_item(self, key_to_item):
+        item_to_load = self.local_ledger[key_to_item]
+        
+        if os.path.exists(item_to_load['local_path']): #cached
+            logging.info(f"Image {item_to_load['local_path']} cached. Loading from disk!")
+            return load_image(item_to_load['local_path'])
+                
+        img = load_image_from_url(item_to_load['url'])
+        img = self.prepare_image(img)
+
+        cv2.imwrite(item_to_load['local_path'], img) # cache it for the future
+        return img
+
+    def next_item(self):
+        key_to_item = random.choice(list(self.local_ledger.keys()))
+        logging.info(f"Loading {key_to_item}")
+        return self.load_item(key_to_item)
 
 
+# def display_image(image, monitor_width, monitor_height):
 
-
-def load_image(image_path):
-    img = cv2.imread(image_path)
-    if img is None:
-        logging.error(f"{image_path} not found.")
-    else:
-        logging.info(f"{image_path} loaded successfully.")
-    
-    return img
-
-
-def prepare_image(image, monitor_width, monitor_height):
-     # Get image dimensions
-    image_height, image_width, _ = image.shape
-
-    # Calculate aspect ratio
-    image_aspect_ratio = image_width / image_height
-    monitor_aspect_ratio = monitor_width / monitor_height
-
-    # Calculate new dimensions to fill the screen while maintaining aspect ratio
-    if image_aspect_ratio > monitor_aspect_ratio:
-        new_width = monitor_width
-        new_height = int(monitor_width / image_aspect_ratio)
-    else:
-        new_width = int(monitor_height * image_aspect_ratio)
-        new_height = monitor_height
-
-    # Resize the image
-    resized_image = cv2.resize(image, (new_width, new_height))
-
-    # Calculate padding to center the image
-    pad_x = (monitor_width - new_width) // 2
-    pad_y = (monitor_height - new_height) // 2
-
-     # Create a black background to fill the entire screen
-    background = np.zeros((monitor_height, monitor_width, 3), dtype=np.uint8)
-
-    # Place the resized image in the center of the background
-    background[pad_y:pad_y+new_height, pad_x:pad_x+new_width] = resized_image
-
-    return background
-
-def display_image(image, monitor_width, monitor_height):
-
-    image_to_display = prepare_image(image, monitor_width, monitor_height)
+#     image_to_display = prepare_image(image, monitor_width, monitor_height)
    
-    # Create a full-screen window
-    cv2.namedWindow('Image', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty('Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#     # Create a full-screen window
+#     cv2.namedWindow('Image', cv2.WND_PROP_FULLSCREEN)
+#     cv2.setWindowProperty('Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-    # Display the image
-    cv2.imshow('Image', image_to_display)
+#     # Display the image
+#     cv2.imshow('Image', image_to_display)
 
-    # Wait for a key press
-    cv2.waitKey(0)
+#     # Wait for a key press
+#     cv2.waitKey(0)
 
-    # Close all OpenCV windows
-    cv2.destroyAllWindows()
+#     # Close all OpenCV windows
+#     cv2.destroyAllWindows()
 
 class Monitor:
     def __init__(self):
@@ -187,30 +254,30 @@ if __name__ == '__main__':
     
     monitor_width, monitor_height = monitor.device.width, monitor.device.height
 
-    image_path = '/home/memorylane/MemoryLane/stamp.jpg'
     data_fountain = URLFountain('http://memorylane.canton.cat/sammamish2300')
+
+    media_repository = MediaRepository(monitor_width, monitor_height)
+    media_repository.load_local_ledger()
+    media_repository.update(data_fountain)
+    media_repository.save_local_ledger()
 
     image_queue = queue.Queue(maxsize=QUEUE_SIZE)
 
     # Create image fetcher thread
-    fetcher = ImageFetcher(image_queue, data_fountain.items)
+    fetcher = ImageFetcher(image_queue, media_repository)
     fetcher.daemon = True
     fetcher.start()
-
-    # #image = load_image(image_path)
    
     # Create a full-screen window
     cv2.namedWindow('Image', cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty('Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    items_to_display = []
+ 
     try:
         while(True):
             image = image_queue.get()
-            image_to_display = prepare_image(image, monitor_width, monitor_height)
         
             # Display the image
-            cv2.imshow('Image', image_to_display)
+            cv2.imshow('Image', image)
             
             start_time = time.time()
             while time.time() - start_time < 30:
